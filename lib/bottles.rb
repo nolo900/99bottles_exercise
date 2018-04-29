7d2a9c00ffe7f0289feb6d7b2321da772961ef42
Austin Miles <abm@austinbmiles.com>
2018-04-28 19:51:04 -0500

    a couple minutes later...

--- lib/bottles.rb
+++ lib/bottles.rb
@@ -7,16 +7,14 @@ class Bottles
 	def pluralize_if_needed(word)
 	end
 
-
-
 	def verse(start_amt)
 		next_amt = start_amt - 1 
-		next_vessel = next_amt >= 1 ? 'bottles' : 'bottle'
-		puts next_amt
+		start_vessel = start_amt > 1 ? 'bottles' : 'bottle'
+		next_vessel = next_amt > 1 ? 'bottles' : 'bottle'
 
 		verse =	<<-VERSE
-#{start_amt} bottles of beer on the wall, #{start_amt} bottles of beer.
-Take one down and pass it around, #{next_amt} ${next_vessel} of beer on the wall.
+#{start_amt} #{start_vessel} of beer on the wall, #{start_amt} #{start_vessel} of beer.
+Take one down and pass it around, #{next_amt} #{next_vessel} of beer on the wall.
 VERSE
 	end
 end
